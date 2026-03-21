c323d6e151bcb8e152ab7fba519c502a39990855
200~juandiegoosorio00001 <juan.osorio79@correo.tdea.edu.co>
2026-03-21 10:22:37 -0500

    Post y delete ok

--- vacantes/views.py
+++ vacantes/views.py
@@ -10,21 +10,17 @@ class VacanteViewSet(viewsets.ModelViewSet):
     queryset = Vacante.objects.all()
     serializer_class = VacanteSerializer
 
+    #FILTROS
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['estado', 'area']
+    search_fields = ['titulo', 'descripcion']
+
+    #PERMISOS
     def get_permissions(self):
         if self.action in ['create', 'update', 'partial_update', 'destroy']:
             return [IsAuthenticated(), IsReclutador()]
         return [IsAuthenticated()]
 
+    # GUARDAR USUARIO
     def perform_create(self, serializer):
         serializer.save(creado_por=self.request.user)
-from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import filters
-
-class VacanteViewSet(viewsets.ModelViewSet):
-    queryset = Vacante.objects.all()
-    serializer_class = VacanteSerializer
-
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
-
-    filterset_fields = ['estado', 'area']  
-    search_fields = ['titulo', 'descripcion']  
